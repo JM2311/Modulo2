c6277b7488f26a18d0aef6585b3449691f468069
JM2311 <juan_morales2311@hotmail.com>
2022-03-23 20:44:42 -0300

    bucles y arreglos

---
+++ Clase 7/arreglos.js
@@ -0,0 +1,18 @@
+// Array
+// let some = ['Juan',10,true,false]
+
+// const frutas = ["Manzana", "Naranja", ["Frutilla", "Pomelo"], "Banana"];
+
+// console.log(frutas);
+
+//Para recorrer un array dentro de otro array, es preferible guardarlo en una variable asi es mas legible
+// const someFrutas = frutas[2];
+
+// console.log(`La fruta preferida es : ${someFrutas[1]}`);
+
+//Recorrer un array con el bucle for
+const frutas = ["Manzana", "Naranja", "Frutilla", "Pomelo", "Banana"];
+
+for (let i = 0; i < frutas.length; i++) {
+  console.log(frutas[i]);
+}
---
+++ Clase 7/bucles.js
@@ -0,0 +1,22 @@
+/*
+while (condition) {
+  Codigo q quiero q se repita si se cumple la condicion  
+}
+*/
+
+let i = 1;
+
+while (i <= 100) {
+  console.log(` el numero es ${i}`);
+  i++;
+}
+
+let j = 5;
+do {
+  console.log(`${j} es menor que 2`);
+  j++;
+} while (j < 2);
+
+for (let k = 1; k < 10; k++) {
+  console.log(`es el numero ${k}  `);
+}
